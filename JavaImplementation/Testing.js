aa045f2e33683e83fae13694c38c23382132ee9b
NoraSensei <106131364+NoraSensei@users.noreply.github.com>
2023-01-17 23:53:36 +1000

    Add files via upload

---
+++ JavaImplementation/Testing.js
@@ -0,0 +1,132 @@
+    function shell()
+	{
+		resetPoints();
+		points.push( new Point(954.4692466302978, 467.8642211136777));
+        points.push( new Point(954.3961298986763, 461.0660129990738));
+        points.push( new Point(962.7806966395624, 462.009307486764));
+        points.push( new Point(966.5024513819127, 469.30846393546295));
+        points.push( new Point(965.5852816163786, 477.2849568949581));
+        points.push( new Point(958.521527131415, 481.19400355573345));
+        points.push( new Point(951.6117077294159, 482.19536092069296));
+        points.push( new Point(943.1606551317911, 480.59428435613694));
+        points.push( new Point(936.6896407897135, 473.71496489231254));
+        points.push( new Point(934.6132775102305,458.27728384510726));
+        points.push( new Point(943.027799876245,443.2921165205251));
+        points.push( new Point(961.4352912435504,435.7949531504923));
+        points.push( new Point(983.1790146328509,444.610831186951));
+        points.push( new Point(989.2526887722739, 467.0521699422029));
+        points.push( new Point(986.177967872088, 491.2766962704973));
+        points.push( new Point(974.8742592891293, 501.7533059695772));
+        points.push( new Point(949.1697708281469,511.44822797237595));
+        points.push( new Point(925.4215367824105,510.47097154894436));
+        points.push( new Point(904.8366664135814,489.841611917819));
+        points.push( new Point(897.3947286552481,467.69761669517203));
+        points.push( new Point(899.3796543219089,435.184228365564));
+        points.push( new Point(923.8195013111305, 409.7306783278209));
+        points.push( new Point(956.4692625122202,399.0505473472411));
+        points.push( new Point(1005.089370775939,407.27311974004124));
+        points.push( new Point(1021.8484167369758,426.1385885877589));
+        points.push( new Point(1035.64412203042,459.9341666378448));
+        points.push( new Point(1035.3296499908952,495.0189146857726));
+        points.push( new Point(1023.8286917523071, 520.1192328956745));
+        points.push( new Point(1005.1705573249836,548.5775518036752));
+        points.push( new Point(983.5351527067305,561.4770945121746));
+        points.push( new Point(958.6683047184365, 577.4853519689904));
+        points.push( new Point(918.1612853081558,569.6631985022128));
+        points.push( new Point(880.6802946557384,552.0844186197035));
+        points.push( new Point(851.3576482151059,525.5409547463368));
+        points.push( new Point(828.1669839845848, 489.8498168380028));
+        points.push( new Point(820.7729619063151,456.022783026375));
+        points.push( new Point(822.4087539668872,430.228207713777 ));
+        points.push( new Point(841.0083511558961,390.99514562296383));
+        points.push( new Point(868.8994873972505,356.5931631955229));
+        points.push( new Point(910.2234685487299,330.04568131307803));
+        points.push( new Point(957.7062091214741,318.6644471683855));
+        points.push( new Point(1015.3066444318376,322.76783609896233));
+        points.push( new Point(1063.1049261942035, 346.52387545038414));
+        points.push( new Point(1119.685157251728, 416.71152613897965));
+        points.push( new Point(1129.002058339944,464.1769693067413));
+        points.push( new Point(1129.354122235284, 512.2493238825566));
+        points.push( new Point(1126.6832468690493,570.0925007799658));
+        points.push( new Point(1105.6983122622137,613.2011916118233));
+        points.push( new Point(1073.584055975098,642.7045769014929));
+        points.push( new Point(1034.2026787058962, 670.345556491896));
+        points.push( new Point(978.0238640976038,679.3748997517915));
+        points.push( new Point(891.795774675738,695.5912915560524));
+        points.push( new Point(820.4158894012775,675.2283404786007));
+        points.push( new Point(764.1356259996231,639.8467997908488));
+        points.push( new Point(709.3578556785363, 589.2248555166934));
+        points.push( new Point(669.5973214081869, 519.2316168328703));
+        points.push( new Point(649.9839165209933, 440.6261325756562));
+        points.push( new Point(643.8656772840485,376.67689915763714));
+        points.push( new Point(657.4713957257835,308.7837651287436));
+        points.push( new Point(691.8596638899103, 237.08552306011342));
+        points.push( new Point(738.21410130088,185.1197012263504));
+        points.push( new Point(821.923563230026,146.54541617036796));
+        points.push( new Point(926.5683429795656,119.32393282423236,));
+        points.push( new Point(1053.312349121577,123.95846898574204));
+        points.push( new Point(1165.7297846171102, 164.13287207859622));
+        points.push( new Point(1279.1746386438065,272.4449228810781));
+        points.push( new Point(1307.7878440780955,397.15112655716484));
+        points.push( new Point(1331.1221837468458,533.7454476902411));
+        points.push( new Point(1330.7657743824868,653.087930196953));
+        points.push( new Point(1275.0773675455234,779.6818080593374));
+        points.push( new Point(1190.5831383769735, 857.2757661702897));
+        points.push( new Point(1055.8906258665545,904.365648342081));
+        points.push( new Point(879.9490918007194,924.8174125273379));
+        points.push( new Point(708.7963425258465,897.6676654241742));
+        points.push( new Point(534.5702508595416,837.1854053390568));
+        points.push( new Point(418.24114469069673,707.3951551743343));
+        points.push( new Point(357.4446057167509, 597.751343685943));
+        points.push( new Point(324.94575423711564,425.4582659335966));
+        points.push( new Point(322.1488724951976,284.6142916064641));
+        points.push( new Point(351.80376976984684,111.66588936629269));
+        points.push( new Point(467.97167241418737,2.962645194864006));
+        // points.push( new Point(1556, 690));
+		redraw();
+	}
+
+
+    function triangle()
+    {
+        resetPoints();
+        points.push( new Point(932.8461636016475, 557.1115471882639));
+        points.push( new Point(935.8821339432727, 942.2918569027747));
+        points.push( new Point(1465.0443082184706, 244.1228340789203));
+        points.push( new Point(469.99207126983964, 248.15132104521805));
+        points.push( new Point(932.8461636016475, 557.1115471882639));
+        redraw();
+    }
+
+
+    function rectangle()
+    {
+        resetPoints();
+        points.push( new Point(942.552917483866,458.6594574243211));
+        points.push( new Point(943.025673605859,6.13960248137339));
+        points.push( new Point(945.2505207444658,919.694308063269));
+        points.push( new Point(1919.863460906058,446.2785307515065));
+        points.push( new Point(0.48423789640585313,449.40935929544565));
+        points.push( new Point(3.323032921809639,9.697571178227498));
+        points.push( new Point(1919.9254826622237,15.219739708209856));
+        points.push( new Point(1919.7416765255432,927.4452340046713));
+        points.push( new Point(0.31483343151585363,928.0990074782962));
+        points.push( new Point(26,943));
+        redraw();
+    }
+
+    
+    function heart()
+    {
+        resetPoints();
+        points.push( new Point(467.97167241418737,2.962645194864006));
+        redraw();
+    }
+
+
+    function fish()
+    {
+        resetPoints();
+        points.push( new Point(467.97167241418737,2.962645194864006));
+        redraw();
+    }
